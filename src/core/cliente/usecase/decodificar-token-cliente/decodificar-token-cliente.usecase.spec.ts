b726c43c2a47af828f3a2ee1310bc00279245d2c
MMatiazzo <mathmatiazzo@gmail.com>
2024-05-10 18:48:41 -0300

    feat: teste

---
+++ src/core/cliente/usecase/decodificar-token-cliente/decodificar-token-cliente.usecase.spec.ts
@@ -0,0 +1,21 @@
+describe(`suite test action`, () => {
+  it('teste 1', () => {
+    console.log('teste 1');
+    expect(1).toBe(1);
+  });
+
+  it('teste 2', () => {
+    console.log('teste 1');
+    expect(1).toBe(1);
+  });
+
+  it('teste 3', () => {
+    console.log('teste 1');
+    expect(1).toBe(1);
+  });
+
+  it('teste 4', () => {
+    console.log('teste 1');
+    expect(1).toBe(1);
+  });
+});
